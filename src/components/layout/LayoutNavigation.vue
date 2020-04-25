872e5cb0c32876fd8195408f44ce5f3e68fff570
paulor8bit <thunder_momon@hotmail.com>
2020-04-25 16:02:38 -0300

    Controle de Gastos

---
+++ src/components/layout/LayoutNavigation.vue
@@ -0,0 +1,59 @@
+<template>
+<nav class="nav flex-column justify-content-between">
+  <div>
+  <router-link
+  exact
+  :key="i"
+  class="nav-link"
+  exact-active-class="active"
+  :to="{name: router.name}"
+  v-for="(router, i) in routerLinks"
+  >
+  <i class="fa" :class="`fa-${router.meta.icon}`"></i>
+    {{router.meta.title}}
+  </router-link>
+
+  <hr>
+  <layout-new-expense/>
+  </div>
+  <layout-logout />
+ </nav>
+</template>
+
+<script>
+import LayoutLogout from './LayoutLogout'
+import LayoutNewExpense from './LayoutNewExpense'
+
+export default {
+  components: {
+    LayoutLogout,
+    LayoutNewExpense
+  },
+  computed: {
+    routerLinks () {
+      return this.$router.options.routes.filter(r => r.name !== 'Login')
+    }
+  }
+
+}
+</script>
+
+<style scoped lang="scss">
+  .nav {
+    margin-left: -15px;
+    width: calc(100% + 30px);
+    height: calc(100vh - 50px);
+    .nav-link {
+      color: var(--white);
+      &.active {
+        color: var(--featured);
+        background-color: transparent;
+      }
+      &:hover {
+        background-color: var(--featured);
+        color: var(--dark)
+      }
+    }
+
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import FirebaseVue from './firebase'
+import './assets/scss/app.scss'
+import './ultils/directives'
+
+Vue.use(FirebaseVue)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
